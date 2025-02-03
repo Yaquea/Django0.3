b43a46f1360063f061df1ed70fe38df34de12853
yaquea <gerardoyou30@gmail.com>
2025-02-03 17:35:27 -0600

    feat: login only username

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.main, name=("main")),
-    path('/signup', views.signup ,name=("signup"))
+    path('signup/', views.signup ,name=("signup")),
+    path('login/', views.login ,name=("login")),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth import login as auth_login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignupForm
 
 def main(request):
@@ -18,4 +20,18 @@ def signup(request):
             user = form.save(commit=False)  # Don't save yet
             user.password = make_password(form.cleaned_data['password1'])  # Hash password
             user.save()  # Now save to DB
-            return redirect('main')  # Redirect to login page or another view
+            return redirect('login')  # Redirect to login page or another view
+        
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html', {'form': AuthenticationForm()})
+    else:
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('main')
+        else:
+            # Extract error message from form or use a default
+            error = "Invalid username or password. Please try again."
+            return render(request, 'login.html', {'form': form, 'error': error})
